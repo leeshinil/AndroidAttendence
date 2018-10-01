74489dd0f5816c083443cf5d9f1f654168b93254
ShinIl Lee <shinillee@ShinIlui-MacBook-Pro.local>
2018-10-01 17:30:11 +0900

    first commit

---
+++ app/src/main/java/com/example/shinillee/attendance/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.shinillee.attendance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button login_btn = findViewById(R.id.loginButton);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), check.class);
+                startActivity(intent);
+            }
+        });
+//
+//        Button signup_btn = findViewById(R.id.signupButton);
+//        signup_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(),sighup.class);
+//                startActivity(intent);
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/example/shinillee/attendance/check.java
@@ -0,0 +1,36 @@
+package com.example.shinillee.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class check extends AppCompatActivity {
+    long mNow;
+    Date mDate;
+    SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check);
+        //bind view
+        mTextView = (TextView) findViewById(R.id.checkTime);
+        //while(true) {
+            mTextView.setText(getTime());
+        //}
+
+    }
+    private String getTime(){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return mFormat.format(mDate);
+    }
+
+}
